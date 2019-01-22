378b778b8aa5d1690f8dcf5d9e0828da36b3fee1
ash37373 <ash37373@gmail.com>
2019-01-22 22:48:38 +0900

    정렬방법
    array , object

--- js/index.js
+++ js/index.js
@@ -26,3 +26,38 @@ console.log(b, a);
 //Float - 숫자인데 소수점을 가지고있음
 // const wat = 55.1;
 // console.log(wat);
+
+
+//정렬방법(array)
+// array = 데이터를 저장하는곳인데 리스트같이 저장
+//const daysOfWeek = ["Mon","Thu", "Wed", "Thu", "Fri", "Sat", "Sun", true, 3.14];
+//console.log(daysOfWeek);
+//console.log(daysOfWeek[2]);//어레이법칙 []사용하여 해당하는 위치 데이터 고를수있따.
+// 위에 방법이 array에 데이터 넣는 첫번째 방법.
+
+
+//두번째 정렬방법(object)
+const hwanInfo = {
+  name:"Hwan",
+  age:34,
+  gender:"Male",
+  isHandsome:true,
+  favMovies:["Oldboy","Titanic"],
+  favFood:[
+    {
+      name:"Kimchi",fatty:false
+    },
+    {
+      name:"Cheese",fatty:true
+    }
+  ]
+
+};
+
+//console.log(hwanInfo);
+//hwanInfo.name = "ChuChu";
+//console.log(hwanInfo.name);
+console.log(hwanInfo.favFood);
+
+
+//만약 DB에서 가져온 리스트라면 array 선택하고, 만약 데이터 합쳐서 만들어야 한다면 Object를 array안에 넣을 수 있다.
