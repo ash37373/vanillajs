6b304b9456fa2ff6b0507f1e931cdf933ded3800
ash37373 <ash37373@gmail.com>
2019-01-22 21:52:35 +0900

    variables

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
 // alert('Im Working. Im JS. Im Beautiful. Im worth it');
 console.log('Im Working. Im JS. Im Beautiful. Im worth it');
-
-let a = 221;
+// let, const, var
+//const 는 상수
+const a = 221;
 let b = a-5;
-a=4;
+//a=4;
 console.log(b, a);
+
+
+
+//const what = ???????
+// string - 스트링은 보통 텍스트 스트링을 말한다 - "sunghwan"
+//const what = "sunghwan";
+// console.log(what);
+
+
+//Booliean - true or false;
+// const wat = true;
+// console.log(wat);
+
+//Number - 숫자
+// const wat = 666;
+// console.log(wat);
+
+//Float - 숫자인데 소수점을 가지고있음
+// const wat = 55.1;
+// console.log(wat);
